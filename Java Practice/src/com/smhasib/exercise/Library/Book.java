a062f2cea0b981ac5e3a735c8b4d385b9651c7cc
S M Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2024-08-01 15:48:23 +0600

    New Additions

--- Java Practice/src/com/smhasib/exercise/Library/Book.java
+++ Java Practice/src/com/smhasib/exercise/Library/Book.java
@@ -2,5 +2,5 @@ package com.smhasib.exercise.Library;
 
 public class Book extends LibraryItem {
     protected String ISBN;
-
+    
 }
--- Java Practice/src/com/smhasib/exercise/Library/DVD.java
+++ Java Practice/src/com/smhasib/exercise/Library/DVD.java
@@ -3,9 +3,16 @@ package com.smhasib.exercise.Library;
 public class DVD extends LibraryItem {
     protected String duration;
 
-    public DVD(String duration, String itemID) {
-        this.duration = duration;
+    public DVD(String itemID, String title, String author, String duration) {
         this.itemID = itemID;
+        this.title = title;
+        this.author = author;
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "DVD [itemID=" + itemID + ", duration=" + duration + ", title=" + title + ", author=" + author + "]";
     }
 
 }
